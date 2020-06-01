6884cf2d97faf610c25dc86b705476efc08fb098
yuriBaza23 <yuribaza2321@gmail.com>
2020-06-01 03:04:53 -0300

    listen

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const app = new express();
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.listen(port, () => console.log('Started'));
