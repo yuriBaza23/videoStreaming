f648db38a58a28726d70eaff51e10820227b0af9
yuriBaza23 <yuribaza2321@gmail.com>
2020-06-01 03:18:37 -0300

    get method

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ const io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.redirect('index.html')
+})
+
 app.use(express.static(__dirname + "/public"));
 app.listen(port, () => console.log('Started'));
